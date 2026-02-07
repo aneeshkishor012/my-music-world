162622b96d3767b39015617d4ab62191bd2537b1
Aneesh Kishor K <114078154+aneeshLinkEZ@users.noreply.github.com>
2026-02-07 23:58:12 +0530

    style changes

--- app/ui/components/BottomNavBar.tsx
+++ app/ui/components/BottomNavBar.tsx
@@ -19,7 +19,7 @@ export default function BottomNavBar() {
     ];
 
     return (
-        <nav className="fixed bottom-10 left-1/2 -translate-x-1/2 w-[90%] z-40 md:hidden flex items-center justify-around px-4 py-3 rounded-2xl  bg-blue/10 backdrop-blur-xl border border-blue-950  shadow-[0_8px_30px_rgba(0,0,0,0.4)] ">
+        <nav className="fixed bottom-10 left-1/2 -translate-x-1/2 w-[90%] z-40 md:hidden flex items-center justify-around px-2 py-2 rounded-2xl  bg-blue/10 backdrop-blur-xl border border-blue-950  shadow-[0_8px_30px_rgba(0,0,0,0.4)] ">
             {navItems.map((item) => {
                 const Icon = item.icon;
 
@@ -32,7 +32,7 @@ export default function BottomNavBar() {
                     <Link
                         key={item.href}
                         href={item.href}
-                        className={` p-3 rounded-xl transition-all duration-300  ${active ? "text-blue-500 bg-blue/10 shadow-[0_0_10px_rgba(96,165,250,0.6)]" : "text-gray-300 hover:text-white"} `}
+                        className={`p-1 rounded-xl transition-all duration-300  ${active ? "text-blue-500 bg-blue/10 shadow-[0_0_10px_rgba(96,165,250,0.6)]" : "text-gray-300 hover:text-white"} `}
                         title={item.label}
                     >
                         <Icon className="w-6 h-6" />
